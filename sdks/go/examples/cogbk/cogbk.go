c472cd6e1cf4d423bd83c8681a644401dd7cfc39
riteshghorse <riteshghorse@gmail.com>
2022-06-13 17:55:55 -0400

    cogbk

--- sdks/go/examples/cogbk/cogbk.go
+++ sdks/go/examples/cogbk/cogbk.go
@@ -18,6 +18,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/graph/window"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/util/stringx"
@@ -26,17 +27,38 @@ import (
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/options/gcpopts"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/x/beamx"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/x/debug"
-	"math/rand"
+	"sort"
+	"strings"
 	"time"
 )
 
 func init() {
-	beam.RegisterFunction(createPair)
+	beam.RegisterFunction(pairWithTs)
+	beam.RegisterFunction(pairWithWindow)
+	beam.RegisterFunction(formatCoGBKResults)
 }
 
-func createPair(ctx context.Context, ts beam.EventTime, element string, emit func(string, string)) {
-	keys := []string{"A", "B", "C"}
-	emit(keys[rand.Intn(3)], element+ts.String())
+func pairWithTs(ctx context.Context, ts beam.EventTime, element string, emit func(string, string)) {
+	emit(ts.String(), element)
+}
+
+func pairWithWindow(ctx context.Context, w beam.Window, ts beam.EventTime, _ string, emit func(string, string)) {
+	emit(ts.String(), fmt.Sprintf("%v", w))
+}
+
+func formatCoGBKResults(key string, tsIter, wIter func(*string) bool) string {
+	var s string
+	var ts, w []string
+	for tsIter(&s) {
+		ts = append(ts, s)
+	}
+	for wIter(&s) {
+		w = append(w, s)
+	}
+	// Values have no guaranteed order, sort for deterministic output.
+	sort.Strings(ts)
+	sort.Strings(w)
+	return fmt.Sprintf("%s; %s; %s", key, strings.Join(ts, ","), strings.Join(w, ","))
 }
 
 func main() {
@@ -52,11 +74,12 @@ func main() {
 	windowed := beam.WindowInto(s, window.NewFixedWindows(time.Second*30), col)
 
 	str := beam.ParDo(s, stringx.FromBytes, windowed)
-	keyed := beam.ParDo(s, createPair, str)
-	debug.Print(s, keyed)
-	transformed := beam.GroupByKey(s, keyed)
+	keyed := beam.ParDo(s, pairWithTs, str)
+	anotherKeyed := beam.ParDo(s, pairWithWindow, str)
+	transformed := beam.CoGroupByKey(s, keyed, anotherKeyed)
 
-	debug.Print(s, transformed)
+	output := beam.ParDo(s, formatCoGBKResults, transformed)
+	debug.Print(s, output)
 	if err := beamx.Run(context.Background(), p); err != nil {
 		log.Exitf(ctx, "Failed to execute job: %v", err)
 	}
