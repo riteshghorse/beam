a4ccf22162e7a53710d43ccfdde56b2584f6aa6e
riteshghorse <riteshghorse@gmail.com>
2024-01-31 11:29:39 -0500

    get dataflow snippet

---
+++ sdks/python/apache_beam/examples/publish_to_topic.py
@@ -0,0 +1,65 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import argparse
+import json
+import logging
+import random
+import time
+
+import apache_beam as beam
+from apache_beam.io import WriteToPubSub
+from apache_beam.options.pipeline_options import PipelineOptions, SetupOptions
+from apache_beam.transforms.periodicsequence import PeriodicImpulse
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ConvertToBytes(beam.DoFn):
+  def process(self, element, *args, **kwargs):
+    value = json.dumps(element).encode('utf-8')
+    _LOGGER.info('encoded_value: %s', value)
+    yield value
+
+
+def run(argv=None):
+  parser = argparse.ArgumentParser()
+  _, pipeline_args = parser.parse_known_args(argv)
+  pipeline_options = PipelineOptions(pipeline_args)
+  pipeline_options.view_as(SetupOptions).save_main_session = True
+  messages = [{
+      'sale_id': i,
+      'customer_id': i,
+      'product_id': i,
+      'quantity': i,
+      'price': i * 100
+  } for i in range(1, 4)]
+  start_time = time.time()
+  end_time = start_time + 1800
+  with beam.Pipeline(options=pipeline_options) as p:
+    _ = (
+        p
+        | 'Stream Data' >> PeriodicImpulse(
+            start_time, end_time, fire_interval=0.2)
+        | 'Create' >> beam.Map(lambda x: messages[random.randint(0, 2)])
+        | 'Convert' >> beam.ParDo(ConvertToBytes())
+        | 'Write to PubSub' >> WriteToPubSub(
+            'projects/google.com:clouddfe/topics/'
+            'riteshghorse-enrichment-example'))
+
+
+if __name__ == '__main__':
+  run()
---
+++ sdks/python/apache_beam/examples/streaming_enrichment.py
@@ -0,0 +1,66 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# pytype: skip-file
+
+import json
+
+import logging
+import time
+
+import apache_beam as beam
+from apache_beam.io import ReadFromPubSub
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.transforms.enrichment import Enrichment
+from apache_beam.transforms.enrichment_handlers.bigtable import BigTableEnrichmentHandler
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class BytesToRow(beam.DoFn):
+  def process(self, element, *args, **kwargs):
+    value = json.loads(element.decode('utf-8'))
+    time.sleep(3)
+    yield beam.Row(**value)
+
+
+def run(save_main_session=True):
+  pipeline_options = PipelineOptions()
+  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+
+  project_id, instance_id, table_id = \
+    "google.com:clouddfe", "beam-test", "riteshghorse-notebook"
+  subscription = \
+    'projects/google.com:clouddfe/subscriptions/' \
+    'riteshghorse-enrichment-example-sub'
+
+  row_key = 'customer_id'
+  bigtable_enrichment = BigTableEnrichmentHandler(
+      project_id, instance_id, table_id, row_key)
+  with beam.Pipeline(options=pipeline_options) as p:
+    _ = (
+        p
+        | "read from pubsub" >> ReadFromPubSub(subscription=subscription)
+        | "BytesToRow" >> beam.ParDo(BytesToRow())
+        | "Enrichment" >> Enrichment(bigtable_enrichment)
+        | "Write" >> beam.Map(lambda x: _LOGGER.info("element: %s", x)))
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
