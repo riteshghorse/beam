2f02d866c94c142ed830e95a0255ebeae03aa4c0
riteshghorse <riteshghorse@gmail.com>
2024-04-23 15:47:47 -0400

    batched handling for redis cache

--- sdks/python/apache_beam/transforms/enrichment_handlers/bigquery.py
+++ sdks/python/apache_beam/transforms/enrichment_handlers/bigquery.py
@@ -14,7 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from typing import Callable, Optional
+from typing import Callable
+from typing import Optional
+from typing import Union
 
 from google.cloud import bigquery
 
@@ -25,13 +27,20 @@ from apache_beam.typehints import List
 QueryFn = Callable[[beam.Row], str]
 
 
-class BigQueryEnrichmentHandler(EnrichmentSourceHandler):
+class BigQueryEnrichmentHandler(EnrichmentSourceHandler[Union[beam.Row,
+                                                              List[beam.Row]],
+                                                        Union[beam.Row,
+                                                              List[beam.Row]]]):
   def __init__(
       self,
       project: str,
-      query_template: Optional[str] = "",
-      fields: Optional[List[str]] = None,
+      query_template: str,
+      fields: List[str],
+      *,
       query_fn: Optional[QueryFn] = None,
+      condition_template: Optional[str] = "",
+      min_batch_size: Optional[int] = None,
+      max_batch_size: Optional[int] = None,
   ):
     """Initialize the `BigQueryEnrichmentHandler`.
 
@@ -40,38 +49,54 @@ class BigQueryEnrichmentHandler(EnrichmentSourceHandler):
       query_template: a raw query template to use for fetching data.
         It must contain the placeholder `{}` to replace it with `fields` to
         fetch values specific to `fields`.
-      fields: Fields present in the input row to substitue in the placeholders
+      fields: Fields present in the input row to substitute in the placeholders
         in query_template.
     """
     self.project = project
     self.query_template = query_template
     self.fields = fields
     self.query_fn = query_fn
+    self.condition_template = condition_template
+    self._batching_kwargs = {}
+    if min_batch_size is not None:
+      self._batching_kwargs['min_batch_size'] = min_batch_size
+    if max_batch_size is not None:
+      self._batching_kwargs['max_batch_size'] = max_batch_size
 
   def __enter__(self):
     self.client = bigquery.Client(project=self.project)
 
-  def __call__(self, request: beam.Row, *args, **kwargs):
-    if self.query_fn:
-      query = self.query_fn(request)
-    else:
-      request_dict = request._asdict()
-      values = list(map(request_dict.get, self.fields))
+  def __call__(self, request: Union[beam.Row, List[beam.Row]], *args, **kwargs):
+    if isinstance(request, beam.Row):
+      if self.query_fn:
+        # if a query_fn is provided then it return a list of values
+        # that should be populated into the query template string.
+        values = self.query_fn(request)
+      else:
+        request_dict = request._asdict()
+        values = list(map(request_dict.get, self.fields))
+      # construct the query.
       query = self.query_template.format(*values)
-      if None in values:
-        raise ValueError
-    try:
-      result = self.client.query(query=query).result()
-      response_dict = {}
-      for r in result:
-        response_dict = dict(zip(r.keys(), r.values()))
-    except Exception:
-      raise RuntimeError
-    return request, beam.Row(**response_dict)
+      try:
+        # make a call to bigquery. Probably use QueryParameters to prevent
+        # attacks like SQL injection.
+        result = self.client.query(query=query).result()
+        response_dict = {}
+        for r in result:
+          response_dict = dict(zip(r.keys(), r.values()))
+      except Exception:
+        raise RuntimeError
+      return request, beam.Row(**response_dict)
+    else:
+      # handle the batching case here.
+      pass
 
   def __exit__(self, exc_type, exc_val, exc_tb):
     self.client.close()
 
-  def get_cache_key(self, request: beam.Row) -> str:
-    # for testing, just use the first field for caching
-    return request._asdict()[self.fields[0]]
+  def get_cache_key(self, request: Union[beam.Row, List[beam.Row]]) -> str:
+    # for input of list of rows, return a list of cache keys
+    if isinstance(request, beam.Row):
+      return "row_key"
+    else:
+      return ['row_key']
